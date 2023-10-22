d1abf147a0ce0e92b36a14e02be6488f2547fc2b
soiloui <soilouiem@gmail.com>
2023-10-22 22:01:18 +0200

    Inital db setup

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./db/config.js";
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`server is running on port: ${port}`);
-});
+serverInit();
+
+async function serverInit() {
+  try {
+    await connectDB();
+    app.listen(port, () => {
+      console.log(`server is running on port: ${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
